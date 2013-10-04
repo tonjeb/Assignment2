a77a89dd2657e7c1f31b42e8fec9c91611d7fdcf
olagrasli <ola.grasli@gmail.com>
2013-10-04 21:43:10 +0200

    Fixed showactivity

--- src/no/livedata/funrun/app/funrun/ShowActivity.java
+++ src/no/livedata/funrun/app/funrun/ShowActivity.java
@@ -5,13 +5,12 @@ import java.util.ArrayList;
 import no.livedata.funrun.app.funrun.library.DatabaseHandler;
 import no.livedata.funrun.app.funrun.library.Act;
 import no.livedata.funrun.app.funrun.library.Lap;
-import no.livedata.funrun.app.funrun.library.ActivityAdapter;
 import android.os.Bundle;
 import android.app.Activity;
 import android.widget.ListView;
 
 
-public class ShowActivity {
+public class ShowActivity extends Activity {
 	ListView activityView;
 	ActivityAdapter adapter;
 	ArrayList<Act> activityList = new ArrayList<Act>();
